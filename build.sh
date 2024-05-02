dd84141189f7c216b4e0d5902ada53367582f480
TrixieUA <98626846+TrixieUA@users.noreply.github.com>
2024-05-02 23:52:04 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -16,8 +16,8 @@ RELEASE="$(rpm -E %fedora)"
 #rpm-ostree install screen
 curl -Lo /etc/yum.repos.d/trixieua-deck.repo https://copr.fedorainfracloud.org/coprs/trixieua/deck/repo/fedora-$(rpm -E %fedora)/trixieua-deck-fedora-$(rpm -E %fedora).repo && \
 
-rpm-ostree refresh-md --force
-rpm-ostree update
+sudo dnf reinstall gamescope xorg-x11-server-Xwayland 
+
 # this would install a package from rpmfusion
 # rpm-ostree install vlc
 
