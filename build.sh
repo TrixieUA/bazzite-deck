a012153d32b511164b41b1cb91c52ea7b89832f3
TrixieUA <ldelovka4@gmail.com>
2024-06-13 10:55:09 +0300

    Add klassy kde theme

--- build.sh
+++ build.sh
@@ -7,6 +7,7 @@ RELEASE="$(rpm -E %fedora)"
 # Enable copr repo
 curl -Lo /etc/yum.repos.d/trixieua-deck.repo https://copr.fedorainfracloud.org/coprs/trixieua/deck/repo/fedora-$(rpm -E %fedora)/trixieua-deck-fedora-$(rpm -E %fedora).repo?arch=x86_64 && \
 curl -Lo /etc/yum.repos.d/g3tchoo-prismlauncher.repo https://copr.fedorainfracloud.org/coprs/g3tchoo/prismlauncher/repo/fedora-$(rpm -E %fedora)/g3tchoo-prismlauncher-fedora-$(rpm -E %fedora).repo && \
+curl -Lo /etc/yum.repos.d/klassy.repo https://download.opensuse.org/repositories/home:paul4us/Fedora_$(rpm -E %fedora)/home:paul4us.repo && \
 # Replace packages
 rpm-ostree override replace \
   --experimental \
@@ -20,4 +21,5 @@ rpm-ostree install \
   java-latest-openjdk \
   java-1.8.0-openjdk \
   java-17-openjdk \
-  java-21-openjdk
+  java-21-openjdk \
+  klassy
