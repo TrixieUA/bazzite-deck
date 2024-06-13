b7d1f0f7c6ea770960b590a9e4dc57b7f9eaf945
TrixieUA <ldelovka4@gmail.com>
2024-06-13 11:41:15 +0300

    Add bibata cursor theme

--- build.sh
+++ build.sh
@@ -8,6 +8,7 @@ RELEASE="$(rpm -E %fedora)"
 curl -Lo /etc/yum.repos.d/trixieua-deck.repo https://copr.fedorainfracloud.org/coprs/trixieua/deck/repo/fedora-$(rpm -E %fedora)/trixieua-deck-fedora-$(rpm -E %fedora).repo?arch=x86_64 && \
 curl -Lo /etc/yum.repos.d/g3tchoo-prismlauncher.repo https://copr.fedorainfracloud.org/coprs/g3tchoo/prismlauncher/repo/fedora-$(rpm -E %fedora)/g3tchoo-prismlauncher-fedora-$(rpm -E %fedora).repo && \
 curl -Lo /etc/yum.repos.d/klassy.repo https://download.opensuse.org/repositories/home:paul4us/Fedora_$(rpm -E %fedora)/home:paul4us.repo && \
+curl -Lo /etc/yum.repos.d/peterwu-rendezvous.repo https://copr.fedorainfracloud.org/coprs/peterwu/rendezvous/repo/fedora-$(rpm -E %fedora)/peterwu-rendezvous-fedora-40.repo && \
 # Replace packages
 rpm-ostree override replace \
   --experimental \
@@ -23,4 +24,5 @@ rpm-ostree install \
   java-17-openjdk \
   java-21-openjdk \
   klassy \
-  papirus-icon-theme
+  papirus-icon-theme \
+  bibata-cursor-theme
