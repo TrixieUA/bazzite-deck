3c4c69552332b0588e6c378f269bf438905fdcb6
TrixieUA <ldelovka4@gmail.com>
2024-06-13 15:26:25 +0300

    Add materia theme

--- build.sh
+++ build.sh
@@ -9,6 +9,7 @@ curl -Lo /etc/yum.repos.d/trixieua-deck.repo https://copr.fedorainfracloud.org/c
 curl -Lo /etc/yum.repos.d/g3tchoo-prismlauncher.repo https://copr.fedorainfracloud.org/coprs/g3tchoo/prismlauncher/repo/fedora-$(rpm -E %fedora)/g3tchoo-prismlauncher-fedora-$(rpm -E %fedora).repo && \
 curl -Lo /etc/yum.repos.d/klassy.repo https://download.opensuse.org/repositories/home:paul4us/Fedora_$(rpm -E %fedora)/home:paul4us.repo && \
 curl -Lo /etc/yum.repos.d/peterwu-rendezvous.repo https://copr.fedorainfracloud.org/coprs/peterwu/rendezvous/repo/fedora-$(rpm -E %fedora)/peterwu-rendezvous-fedora-40.repo && \
+curl -Lo /etc/yum.repos.d/trixieua-materia-kde6.repo https://copr.fedorainfracloud.org/coprs/trixieua/materia-kde6/repo/fedora-$(rpm -E %fedora)/trixieua-materia-kde6-fedora-40.repo && \
 # Replace packages
 rpm-ostree override replace \
   --experimental \
@@ -25,4 +26,11 @@ rpm-ostree install \
   java-21-openjdk \
   klassy \
   papirus-icon-theme \
-  bibata-cursor-themes
+  bibata-cursor-themes \
+  kvantum \
+  materia-kde6 \
+  materia-kde6-konsole \
+  materia-kde6-yakuake \
+  materia-kde6-decorations \
+  materia-kde6-kvantum \
+  materia-kde6-wallpapers
